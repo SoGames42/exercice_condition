dbc40167c30e169637bd5ff9cbe27e9a6b4ddcca
Sofia <55157087+SoGames42@users.noreply.github.com>
2023-10-08 18:25:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let a=4;
+let b=4;
+let c=3;
+
+if (a==b){
+    console.log("a est égal à b");
+}
+if (a==c){
+    console.log("a est égal à c");
+}
+else{
+    console.log("égal à rien");
+}
